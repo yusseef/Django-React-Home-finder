4bafac7ae33feec115a93e411a10c91d04af1454
yusseef <yussef.raouf11@gmail.com>
2021-08-07 18:49:20 +0200

    react setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Layout from './hocs/Layout'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './containers/Home'
+import About from './containers/About'
+import Contact from './containers/Contact'
+import Listings from './containers/Listings'
+import ListingDetail from './containers/ListingDetail'
+import Signin from './containers/Signin'
+import SignUp from './containers/SignUp'
+import NotFound from './components/NotFound'
+import './sass/main.scss'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+         <Switch>
+           <Route exact path="/" component={Home} />
+           <Route exact path="/about" component={About} />
+           <Route exact path="/contact" component={Contact} />
+           <Route exact path="/listings" component={Listings} />
+           <Route exact path="/listings/:id" component={ListingDetail} />
+           <Route exact path="/signin" component={Signin} />
+           <Route exact path="/signup" component={SignUp} />
+           <Route component={NotFound} />
+         </Switch>
+      </Layout>
+    </Router>
+     
   );
 }
 
---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h1 className="not-found__heading">404-Not found</h1>
+            <p className="not-found__paragraph">The link u requested not here in our website maybe changed or u entered a wrong address <a  href="/">Return to home page</a> </p>
+            
+        
+        </div>
+    )
+}
+
+export default NotFound
